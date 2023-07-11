88587fe76dec1ea8ff8fd09b606bac32222ef67c
soumya-hub1 <118823679+soumya-hub1@users.noreply.github.com>
2023-07-11 13:48:22 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,1 @@
-aws s3 cp s3://codepipeline-us-west-1-290535631393/codepipeline/BuildArtif/LoginWebApp-1.war /tmp
-sudo mv /tmp/LoginWebApp-1.war /usr/share/tomcat/webapps/LoginWebApp-1.war
-sudo service tomcat restart
+
